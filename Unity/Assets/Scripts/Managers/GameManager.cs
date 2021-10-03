737c28d9488b03d2b82c9d3b73f8565c6f8e0802
Steven Garberg <steven.garberg.jr@gmail.com>
2021-10-03 00:02:42 -0400

    Level setup

---
+++ Unity/Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+using UnityEngine;
+
+public class GameManager : NetworkBehaviour
+{
+    public bool Turn;
+    
+    private void OnServerInitialized()
+    {
+        if (!isServer) return;
+    }
+}
