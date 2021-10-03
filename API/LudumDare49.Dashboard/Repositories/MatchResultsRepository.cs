63c0d753f7715b30f5f788316b36cee50894bbde
Christopher Plummer <cplummer@reservations.com>
2021-10-03 16:16:20 -0400

    Removed unused imports and skeletoned out repos in Blazor dashboard

---
+++ API/LudumDare49.Dashboard/Repositories/MatchResultsRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using LudumDare49.API.Models.Requests;
+using LudumDare49.API.Models.Responses;
+
+namespace LudumDare49.Dashboard.Repositories
+{
+    public class MatchResultsRepository
+    {
+        private string testUrl = "";
+
+        public MatchResultsRepository()
+        {
+            
+        }
+
+        public async Task<PlayerMatchResultResponse> GetById(string id)
+        {
+            throw new NotImplementedException();
+        }
+        
+        public async Task<IEnumerable<MatchResults>> Get()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ API/LudumDare49.Dashboard/Repositories/PlayerRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using LudumDare49.API.Models;
+
+namespace LudumDare49.Dashboard.Repositories
+{
+    public class PlayerRepository
+    {
+        public PlayerRepository()
+        {
+            
+        }
+        
+        public async Task<Player> GetById(string id)
+        {
+            throw new NotImplementedException();
+        }
+        
+        public async Task<IEnumerable<Player>> Get()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
