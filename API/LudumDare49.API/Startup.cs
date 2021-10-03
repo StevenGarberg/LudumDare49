db9d69ff72134e28627dde879d4cdc10885b5937
Christopher Plummer <cplummer@reservations.com>
2021-10-03 01:17:36 -0400

    Deploy with index.html

--- API/LudumDare49.API/Startup.cs
+++ API/LudumDare49.API/Startup.cs
@@ -42,12 +42,19 @@ namespace LudumDare49.API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            app.UseCors(x => x
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .SetIsOriginAllowed(origin => true) // allow any origin
+                .AllowCredentials());
+            
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
             {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "LudumDare49.API v1"));
-            }
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "LudumDare49.API v1");
+                c.RoutePrefix = "";
+            });
 
             app.UseHttpsRedirection();
 
